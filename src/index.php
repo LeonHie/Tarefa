8109170604ba2a5b1e04629a0d999812d72268f1
LeonHie <leohiemischblu@gmail.com>
2025-08-10 23:34:03 -0300

    mySqli instalado

--- src/index.php
+++ src/index.php
@@ -1,5 +1,19 @@
 <?php
     
-    echo "Olá, mundo!"; 
+    echo "Olá, mundo!!"; 
+
+$servername = "localhost"; // Or your database host
+$username = "root";
+$password = "senha123";
+$dbname = "Playlist";
+
+// Create connection using MySQLi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
 ?>
