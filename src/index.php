d4427719f2d4c644cabedafa6bbcb2f5e441c2b1
LeonHie <leohiemischblu@gmail.com>
2025-08-11 01:03:29 -0300

    mySqli Acessando

--- src/index.php
+++ src/index.php
@@ -1,19 +1,38 @@
 <?php
     
-    echo "Olá, mundo!!"; 
+    echo "Olá, mundo!! <br><br>"; 
 
-$servername = "localhost"; // Or your database host
+$servername = "10.0.0.51"; // Or your database host
 $username = "root";
 $password = "senha123";
 $dbname = "Playlist";
 
+echo "Buscando últimas músicas tocadas... <br><br>";
 // Create connection using MySQLi
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo "Falha na conexão!!<br><br>";
+    die("A Conexão Falhou: " . $conn->connect_error);
 }
-echo "Connected successfully";
+else{
+    echo "Resultado:  <br>";
+    $sql = "Select * FROM Musicos";
+    $result = $conn->query($sql);
 
+    if ($result->num_rows > 0){
+        echo "<table border='1'>";
+        echo "<tr><th>Nome</th><th>Sobrenome</th></tr>";
+        while($row = $result->fetch_assoc()){
+            echo "<tr>";
+            echo "<td>" . $row ["Nome"] . "</td>";
+            echo "<td>" . $row ["Sobrenome"] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }else {
+        echo "Nenhuma música encontrada...";
+    }
+}
+echo "<br>..."
 ?>
